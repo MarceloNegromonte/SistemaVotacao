fde7df8118237a7a37f525002d6bfbed2cb8ca2d
MarceloNegromonte <mjncavalcante@gmail.com>
2022-07-06 15:11:24 -0300

    Correcao metodo busca

--- src/main/java/com/apisistemaVotacao/sistemaVotacao/repository/VotoRepository.java
+++ src/main/java/com/apisistemaVotacao/sistemaVotacao/repository/VotoRepository.java
@@ -3,11 +3,9 @@ package com.apisistemaVotacao.sistemaVotacao.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.apisistemaVotacao.sistemaVotacao.model.SessaoVotacao;
 import com.apisistemaVotacao.sistemaVotacao.model.Voto;
 
 @Repository
 public interface VotoRepository extends JpaRepository<Voto, Long> {
 
-	Boolean existsBySessaoVotacaoAndCpfUsuario(SessaoVotacao sessaoVotacao, String cfpUsuario);
 }
