5b738731929f76c71332e6fcf98a5f1d53226319
MarceloNegromonte <mjncavalcante@gmail.com>
2022-07-06 14:56:12 -0300

    Implementacao metodos VotoController

--- src/main/java/com/apisistemaVotacao/sistemaVotacao/controller/VotoController.java
+++ src/main/java/com/apisistemaVotacao/sistemaVotacao/controller/VotoController.java
@@ -1,50 +1,40 @@
 package com.apisistemaVotacao.sistemaVotacao.controller;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.apisistemaVotacao.sistemaVotacao.dto.request.VotoRequisicaoDTO;
+import com.apisistemaVotacao.sistemaVotacao.dto.request.VotoRequestDTO;
 import com.apisistemaVotacao.sistemaVotacao.model.Voto;
 import com.apisistemaVotacao.sistemaVotacao.service.VotoService;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/v1/voto")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "*")
 @Slf4j
-@RequiredArgsConstructor
 public class VotoController {
 
 	@Autowired
 	private VotoService votoService;
 	
-	@GetMapping("/{id}")
-	@ResponseStatus(HttpStatus.OK)
-    public List<Voto> listVotacoes() {
-        return votoService.findAllVotacoes();
-    }
+	@Autowired
+	public VotoController(VotoService votoService) {
+		this.votoService = votoService;
+	}
 	
-    @PostMapping("/{idPauta}/votar")
-    public ResponseEntity<Voto> votar(@PathVariable("idPauta") @RequestBody @Valid Long idPauta, @RequestBody @Valid VotoRequisicaoDTO voto) {
-        votoService.votar(idPauta, voto);
-        log.info("Voto registrado com sucesso!");
-       
-        return ResponseEntity.ok().build();
-    }
+	@PostMapping("/voto")
+	public ResponseEntity<Voto> criarVotacao(@Valid @RequestBody VotoRequestDTO dto) {
+		log.info("Voto registrado com sucesso!");
+		return new ResponseEntity<>(votoService.votacao(dto), HttpStatus.CREATED);
+	}
 	
 }
