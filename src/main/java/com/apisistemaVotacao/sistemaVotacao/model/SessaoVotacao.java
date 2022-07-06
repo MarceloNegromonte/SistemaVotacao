7cbf0a19b529214701bf85db3c2b8908f40b906d
MarceloNegromonte <mjncavalcante@gmail.com>
2022-07-06 14:57:23 -0300

    Implementacao atributos SessaoVotacao

--- src/main/java/com/apisistemaVotacao/sistemaVotacao/model/SessaoVotacao.java
+++ src/main/java/com/apisistemaVotacao/sistemaVotacao/model/SessaoVotacao.java
@@ -1,10 +1,14 @@
 package com.apisistemaVotacao.sistemaVotacao.model;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -13,9 +17,13 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.apisistemaVotacao.sistemaVotacao.model.enums.StatusEnum;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -23,6 +31,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 @Table(name = "tb_sessaoVotacao")
 public class SessaoVotacao {
 
@@ -31,20 +40,26 @@ public class SessaoVotacao {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(name = "DataHoraInicio")
-	private LocalDateTime dataHoraInicio;
+	@Column(name = "Duracao")
+	private Integer duracao;
 	
-	@Column(name = "DataHoraFim")
-	private LocalDateTime dataHoraFim;
-	
-	@Column(name = "Status")
-	private boolean status = true;
+	@Column(name = "Fechado")
+	private Instant fechado;
 	
 	@OneToOne
 	@JoinColumn(name = "id_pauta")
 	private Pauta pauta;
 
-	@OneToMany
-	private List<Voto> votos;
+	@JsonIgnoreProperties("sessaoVotacao")
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = "sessaoVotacao", cascade = CascadeType.ALL)
+	private List<Voto> votos = new ArrayList<>();
+	
+	public boolean fechada() {
+		return this.getFechado() != null && this.getFechado().isBefore(Instant.now());
+	}
+	
+	public boolean aberta() {
+		return getPauta().getStatus().equals(StatusEnum.valueOf("ABERTA"));
+	}
 	 
 }
