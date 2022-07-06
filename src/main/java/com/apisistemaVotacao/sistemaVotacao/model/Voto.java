48c5f84f3597c82deed453c59d2635e7bd93cde0
MarceloNegromonte <mjncavalcante@gmail.com>
2022-07-06 14:57:55 -0300

    Implementacao atributos Voto

--- src/main/java/com/apisistemaVotacao/sistemaVotacao/model/Voto.java
+++ src/main/java/com/apisistemaVotacao/sistemaVotacao/model/Voto.java
@@ -1,52 +1,47 @@
 package com.apisistemaVotacao.sistemaVotacao.model;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 
 import com.apisistemaVotacao.sistemaVotacao.model.enums.VotoStatus;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Data
-@Builder(toBuilder = true)
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString(exclude = "sessaoVotacao")
+@EqualsAndHashCode(callSuper=false)
 @Entity
 @Table(name = "tb_voto")
 public class Voto {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "Id")
 	private Long id;
 	
-    @NotNull(message = "CPF do eleitor obrigatório.")
-    private String cpfUsuario;
-
-    @NotNull(message = "Mensagem de voto é obrigatório e precisa seguir o padrão: SIM/NAO")
-    @Column(name = "Mensagem_voto")
-    @Enumerated(EnumType.STRING)
-    private VotoStatus mensagemVoto;
+    @Column(name = "VotoInstant")
+    private Instant votoInstant;
     
-    @Column(name = "data")
-    private LocalDateTime dataHora;
+    @Column(name = "Voto")
+    private VotoStatus voto;
 
     @ManyToOne
+    @JsonIgnoreProperties("votos")
     @JoinColumn(name = "id_sessao_votacao")
     private SessaoVotacao sessaoVotacao;
 }
