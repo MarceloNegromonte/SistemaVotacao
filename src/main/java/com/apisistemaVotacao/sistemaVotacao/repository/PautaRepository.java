5abb8e8ab4cc9ebeca23c7b62a1d2d902cb35e40
MarceloNegromonte <mjncavalcante@gmail.com>
2022-07-06 14:58:31 -0300

    Implementacao metodos PautaRepository

--- src/main/java/com/apisistemaVotacao/sistemaVotacao/repository/PautaRepository.java
+++ src/main/java/com/apisistemaVotacao/sistemaVotacao/repository/PautaRepository.java
@@ -9,4 +9,6 @@ import com.apisistemaVotacao.sistemaVotacao.model.Pauta;
 public interface PautaRepository extends JpaRepository<Pauta, Long> {
 
 	Optional<Pauta> findById(Long id);
+	
+	Optional<Pauta> findByNome(String id);
 }
