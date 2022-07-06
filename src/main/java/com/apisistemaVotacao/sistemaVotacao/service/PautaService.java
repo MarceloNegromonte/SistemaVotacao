123938742afe98fc4b0a54b1f7e255ea828cc1e0
MarceloNegromonte <mjncavalcante@gmail.com>
2022-07-06 12:47:45 -0300

    Implementacao metodos de alteracao de status, porcentagem, vencedor

--- src/main/java/com/apisistemaVotacao/sistemaVotacao/service/PautaService.java
+++ src/main/java/com/apisistemaVotacao/sistemaVotacao/service/PautaService.java
@@ -1,17 +1,12 @@
 package com.apisistemaVotacao.sistemaVotacao.service;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 
+import org.apache.commons.math3.util.Precision;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
@@ -19,11 +14,9 @@ import org.springframework.stereotype.Service;
 import com.apisistemaVotacao.sistemaVotacao.dto.PautaDTO;
 import com.apisistemaVotacao.sistemaVotacao.exception.NotFoundException;
 import com.apisistemaVotacao.sistemaVotacao.model.Pauta;
-import com.apisistemaVotacao.sistemaVotacao.model.SessaoVotacao;
-import com.apisistemaVotacao.sistemaVotacao.model.Voto;
+import com.apisistemaVotacao.sistemaVotacao.model.enums.StatusEnum;
+import com.apisistemaVotacao.sistemaVotacao.model.enums.VotoStatus;
 import com.apisistemaVotacao.sistemaVotacao.repository.PautaRepository;
-import com.apisistemaVotacao.sistemaVotacao.repository.SessaoVotacaoRepository;
-import com.apisistemaVotacao.sistemaVotacao.repository.VotoRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -32,24 +25,19 @@ import lombok.extern.slf4j.Slf4j;
 @Cacheable(value = "pautas", key = "#pauta.id")
 public class PautaService {
 
-    @Value("${tempo.sessao.votacao.segundos}")
-    private Integer tempoSessaoPadrao;
+	private final PautaRepository pautaRepository;
 	
 	@Autowired
-	private PautaRepository pautaRepository;
+	public PautaService(PautaRepository pautaRepository) {
+		this.pautaRepository = pautaRepository;
+	}
 	
-	@Autowired
-	private VotoRepository votoRepository;
-	
-	@Autowired
-	private SessaoVotacaoRepository sessaoVotacaoRepository;
-	
-	@CacheEvict(value = "pautas", allEntries = true)
-	@Transactional
-    public Pauta criarPauta(Pauta pauta) {
-        return pautaRepository.save(pauta);
-    }
-	
+	@Cacheable(value="pautas")
+    @Transactional
+	public List<Pauta> buscarTodasPautas(){
+		return pautaRepository.findAll();
+	}
+
 	@Cacheable(value="pautas")
     @Transactional
     public PautaDTO buscarPautaPeloID(Long id) {
@@ -62,16 +50,54 @@ public class PautaService {
 
         return PautaDTO.toDTO(pautaOptional.get());
     }
+	
+	@Transactional
+	public Pauta buscaPorNome(String nome) {
+		return pautaRepository.findByNome(nome).orElse(null);
+	}
+	
+	@CacheEvict(value = "pautas", allEntries = true)
+	@Transactional
+	public Pauta criarPauta(Pauta estado) {
+		estado.setStatus(StatusEnum.ABERTA);
+		
+		return pautaRepository.save(estado);
+	}
+	
+	@CacheEvict(value = "pautas")
+	@Transactional
+	public void mudancaStatus(Pauta estado) {
+		estado.setStatus(StatusEnum.FECHADA);
+		
+		pautaRepository.save(estado);
+	}
+	
+	@CacheEvict(value = "pautas")
+	@Transactional
+	public void definirVencedor(Pauta pauta) {
+		if (pauta.getPercentualSim() > pauta.getPercentualNao()) {
+			
+			pauta.setVencedor(VotoStatus.SIM);
+		}else {
+			pauta.setVencedor(VotoStatus.NAO);
+		}
+	}
     
-    public Optional<Pauta> buscarPauta(Long id) {
-        return pautaRepository.findById(id);
+	@CacheEvict(value = "pautas")
+	@Transactional
+	public void definirPercentual(Pauta pauta) {
+        pauta.setPercentualSim(Precision.round(((
+                Double.valueOf(pauta.getQtdVotosSim())/ pauta.getQtdVotos())*100), 2));
+        pauta.setPercentualNao(Precision.round(((
+                Double.valueOf(pauta.getQtdVotosNao())/ pauta.getQtdVotos())*100), 2));
     }
     
-    private Optional<SessaoVotacao> buscarSessaoVotacao(Pauta pauta) {
+	
+   /* private Optional<SessaoVotacao> buscarSessaoVotacao(Pauta pauta) {
         return sessaoVotacaoRepository.findByPauta(pauta);
-    }
+    }*/
     
-    @Transactional
+    /*@Transactional
     public void votar(Long idPauta, Voto voto) {
         SessaoVotacao sessaoVotacao = buscarSessaoVotacao(buscarPauta(idPauta)
                 .orElseThrow(() -> new NotFoundException("PAUTA_NAO_ENCONTRADA")))
@@ -89,9 +115,9 @@ public class PautaService {
         }
 
         votoRepository.save(voto);
-    }
+    }*/
     
-    public Map<String, Long> resultado(Pauta pauta) {
+   /* public Map<String, Long> resultado(Pauta pauta) {
 
         Collection<Voto> votos = buscarSessaoVotacao(pauta).isPresent() ? buscarSessaoVotacao(pauta).get().getVotos() : new ArrayList<>();
 
@@ -100,14 +126,16 @@ public class PautaService {
         resultado.put("NAO", votos.stream().filter(v -> v.getMensagemVoto().toString().equalsIgnoreCase("NAO")).count());
 
         return resultado;
-    }
+    }*/
 
-    public Integer buscarTempoSessaoPadrao() {
+    /*public Integer buscarTempoSessaoPadrao() {
         return tempoSessaoPadrao;
-    }
-    
-    public List<Pauta> buscarPautas() {
-        return pautaRepository.findAll();
-    }
+    }*/
     
 }
+
+/*	@CacheEvict(value = "pautas", allEntries = true)
+@Transactional
+public Pauta criarPauta(Pauta pauta) {
+    return pautaRepository.save(pauta);
+}*/
