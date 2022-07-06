7e0a15fe6d7b8788725cbbda4f3663e4d1603578
MarceloNegromonte <mjncavalcante@gmail.com>
2022-07-06 12:58:36 -0300

    Correcao Metodos busca/criacao

--- src/main/java/com/apisistemaVotacao/sistemaVotacao/controller/PautaController.java
+++ src/main/java/com/apisistemaVotacao/sistemaVotacao/controller/PautaController.java
@@ -1,7 +1,6 @@
 package com.apisistemaVotacao.sistemaVotacao.controller;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
@@ -14,31 +13,52 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.apisistemaVotacao.sistemaVotacao.dto.PautaDTO;
-import com.apisistemaVotacao.sistemaVotacao.dto.request.PautaRequisicaoDTO;
-import com.apisistemaVotacao.sistemaVotacao.dto.response.PautaResponseDTO;
 import com.apisistemaVotacao.sistemaVotacao.model.Pauta;
+import com.apisistemaVotacao.sistemaVotacao.repository.PautaRepository;
 import com.apisistemaVotacao.sistemaVotacao.service.PautaService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
 @RequestMapping("/v1/pauta")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
-@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class PautaController {
 
 	@Autowired
 	private PautaService pautaService;
-    private final ObjectMapper objectMapper;
 	
-	@PostMapping("/criar")
+	@Autowired
+	public PautaController(PautaService pautaService, PautaRepository pautaRepository) {
+		this.pautaService = pautaService;
+	}
+	
+	@GetMapping("/todasPautas")
+	public ResponseEntity<List<Pauta>> buscarTodasPautas(){
+		return ResponseEntity.ok(pautaService.buscarTodasPautas());
+	}
+	
+		@GetMapping(value = "/{id}")
+	    public ResponseEntity<PautaDTO> buscarPautaPeloID(@PathVariable("id") Long id) {
+	        log.debug("Buscando a pauta pelo ID = {}", id);
+	        
+	        return new ResponseEntity<>(pautaService.buscarPautaPeloID(id), HttpStatus.OK);
+	    }
+	
+		@GetMapping
+		public ResponseEntity<Pauta> buscaPorNome(String nome) {
+			return new ResponseEntity<>(pautaService.buscaPorNome(nome), HttpStatus.OK);
+		}
+		
+		@PostMapping("/criar")
+		public ResponseEntity<Pauta> criarPauta(@Valid @RequestBody Pauta pauta) {
+			return new ResponseEntity<>(pautaService.criarPauta(pauta), HttpStatus.CREATED);
+		}
+	
+	/*@PostMapping("/criar")
     public ResponseEntity<PautaResponseDTO> criarPauta(@RequestBody @Valid PautaRequisicaoDTO pautaRequest) {
         log.info("Criando pauta...");
         Pauta pauta = objectMapper.convertValue(pautaRequest, Pauta.class);
@@ -49,29 +69,24 @@ public class PautaController {
 		return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .build();
-    }
+    }*/
 	
-	   @GetMapping(value = "/{id}")
-	   @ResponseStatus(HttpStatus.OK)
-	    public ResponseEntity<PautaDTO> buscarPautaPeloID(@PathVariable("id") Long id) {
-	        log.debug("Buscando a pauta pelo ID = {}", id);
-	        return ResponseEntity.ok(pautaService.buscarPautaPeloID(id));
-	    }
 	   
-	    @GetMapping(value = "/resultado")
+	   
+	   /* @GetMapping(value = "/resultado")
 	    public List<PautaResponseDTO> buscarPautasComResultado() {
 	        log.info("Consultando pautas...");
 	        return pautaService.buscarPautas().stream()
 	                .map(this::getPautaResponse)
 	                .collect(Collectors.toList());
-	    }
+	    }*/
 	    
-	    private PautaResponseDTO getPautaResponse(Pauta pauta) {
+	   /* private PautaResponseDTO getPautaResponse(Pauta pauta) {
 	        PautaResponseDTO pautaResponse = objectMapper.convertValue(pauta, PautaResponseDTO.class);
 	        pautaResponse.setResultado(pautaService.resultado(pauta));
 
 	        return pautaResponse;
-	    }
+	    }*/
 	
 }
 
