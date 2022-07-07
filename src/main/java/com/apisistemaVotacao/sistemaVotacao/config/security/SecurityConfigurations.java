8a2db60d7a1a5c851e1c931f9f750cd802a727de
MarceloNegromonte <mjncavalcante@gmail.com>
2022-07-07 14:13:20 -0300

    Criacao seguranca JWT

--- src/main/java/com/apisistemaVotacao/sistemaVotacao/config/security/SecurityConfigurations.java
+++ src/main/java/com/apisistemaVotacao/sistemaVotacao/config/security/SecurityConfigurations.java
@@ -1,41 +1,60 @@
 package com.apisistemaVotacao.sistemaVotacao.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.apisistemaVotacao.sistemaVotacao.repository.UsuarioRepository;
 
 @EnableWebSecurity
 @Configuration
-public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+public class SecurityConfigurations {
 
-	@Autowired
-	private AutenticacaoService autenticacaoService;
-	
-	//configuracoes de autenticacao
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
-	}
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
 	
 	//configuracoes de autorizacao
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests()
-		.antMatchers(HttpMethod.GET, "/pauta").permitAll()
-		.antMatchers(HttpMethod.GET, "pautas/*").permitAll()
-		.anyRequest().authenticated()
-		.and().formLogin();
-	}
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        .antMatchers(HttpMethod.GET, "/v1/pautas").permitAll()
+        .antMatchers(HttpMethod.GET, "/v1/topicos/*").permitAll()
+        .antMatchers(HttpMethod.POST, "/auth").permitAll()
+        .anyRequest().authenticated()
+        .and().csrf().disable()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class);
+        
+        return http.build();
+    }
 	
 	// configuracaoes de recursos estaticos(CSS, imagens, etc)
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-	}
-	
+    @Bean //corrigir o caminho.
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
+    }
 }
