40ff535e0f9b496bce39358c691aa9abd2059fca
MarceloNegromonte <mjncavalcante@gmail.com>
2022-07-12 05:48:57 -0300

    Ultimos ajustes seguranca

--- src/main/java/com/apisistemaVotacao/sistemaVotacao/config/security/SecurityConfigurations.java
+++ src/main/java/com/apisistemaVotacao/sistemaVotacao/config/security/SecurityConfigurations.java
@@ -10,8 +10,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -39,11 +37,6 @@ public class SecurityConfigurations {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
-    }
-    
-    @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
     }
     
     @Bean
